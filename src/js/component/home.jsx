7a1d4fb24d829d3bd11f87f1716867eb1fe4b92f
Annams02 <155559840+Annams02@users.noreply.github.com>
2024-05-02 18:14:06 +0000

    funciones funcionando

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,47 @@
-import React from "react";
+
+import React, {useState, useEffect}from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [tareas, setTareas] = useState([
+		
+	]);
+
+	const [inputValue, setInputValue] = useState ("");
+	
+	const controlarInput = (e) => {
+		setInputValue(e.target.value) 
+	}
+	const controlarEnter = (e) => {
+		if (e.key === 'Enter') {
+			setTareas(tareas.concat(inputValue));
+		}
+	}
+	
+	const deleteItems = (e) => {
+		setTareas((prevState) =>
+		  prevState.filter((tarea, index) => index !== e)
+		);
+	  };
+	
+
+
 	return (
 		<div className="container">
 			<div className="row justify-content-center">
 				<h1>To Do List</h1>
 				<ul>
-					<li><input type="text" placeholder="What do you need in your to do list?" ></input> </li>
-					<li> Ir a la peluqueria <i className="fa-light fa-trash"></i> </li>
-					<li> Pedido Amazon <i className="fa-light fa-trash"></i> </li>
+					<li><input type="text" onChange={controlarInput} onKeyDown={controlarEnter} value={inputValue} placeholder="What do you need in your to do list?" ></input> </li>
+					{tareas.map((t, index) => <li key={index}>{t} <button onClick={() => deleteItems (index)} className="fa-light fa-trash"></button></li>)}
+			
 				</ul>	
 			</div>
 
+				<div> {tareas.length} tasks</div>
 
-			
-			
-		
 		</div>
 	);
 };
